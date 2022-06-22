f0fa8e117755b0e0393e1fe576229ba0157b3aa4
HP <samat.osmonov@gmail.com>
2022-06-23 00:20:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "name";//0 1 2
+
+        char symbol = s.charAt(2);//index berilet
+        System.out.println(symbol);
+
+        String soz = "Hello word";
+        String newsoz = soz.substring(5);
+
+        System.out.println(newsoz);
+
+        String soz2 = "Moscow";
+        System.out.println(soz2.contains("moscow"));
+
+        String soz3 = "Daniel";
+        System.out.println(soz3.toUpperCase());//oidogo karai
+        System.out.println(soz3.toLowerCase());//yldyiga karai
+
+        String soz4 = "Daniel student";
+        System.out.println(soz4);
+        System.out.println(soz.trim());//boshtuktu alyp salat
+
+        String soz5 = "Hi";
+        String soz6 = "Hi";
+
+        System.out.println(soz5.equals(soz6));// maanisin teksheret
+        System.out.println("Hi".equalsIgnoreCase("Hi"));
+
+        String name = "Java";
+        String name2 = "Hi";
+        String concat
+
+
+
+    }
+
+
+
+
+
+    }
+}
